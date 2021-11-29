6245800e18c9355e760d694f3aaf1787e5d9d907
trentarnold <trentarnold8@gmail.com>
2021-11-29 15:30:44 -0800

    premerge commit

--- moviematcher/src/components/movie-page/movie-page.tsx
+++ moviematcher/src/components/movie-page/movie-page.tsx
@@ -3,21 +3,27 @@ import MovieDetails from './movie-details/movie-details';
 import React, {useState, useEffect} from 'react';
 import APIService from '../../services/APISevice';
 import { Movie } from '../../../../interfaces/MovieInterface';
-
+import { useParams } from 'react-router';
 const MoviePage = () => {
-  const [popularMovies, setPopularMovies] = useState<Movie[]>([])
+  const { id } : any = useParams();
+  const [similarMovies, setSimilarMovies] = useState<Movie[]>([])
   useEffect(() => {
+    let isCancelled = false;
     async function fetchPopular () {
-        const fetchedPopularMovies  = await APIService.getPopularMovies();
-        setPopularMovies(fetchedPopularMovies.results)
+        const fetchedPopularMovies  = await APIService.getSimilarMovies(id);
+        if(!isCancelled) {
+            setSimilarMovies(fetchedPopularMovies.results)
+        }
     }
     fetchPopular()
-
-}, [])
+    return () => {
+        isCancelled = true;
+    }
+  }, [id])
     return (
         <div>
             <MovieDetails />
-            <MovieList movieList = {popularMovies} />
+            <MovieList movieList = {similarMovies} />
         </div>
     )
 }
