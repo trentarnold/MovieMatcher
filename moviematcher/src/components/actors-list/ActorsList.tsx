195df471d470c84ce9227ee6827cb7ed009c76d7
Marshal Fisher <marshal_fisher@yahoo.com>
2021-11-29 17:10:37 -0600

    fixed repeting actor bug, actor-thumb lists role now

--- moviematcher/src/components/actors-list/ActorsList.tsx
+++ moviematcher/src/components/actors-list/ActorsList.tsx
@@ -4,15 +4,24 @@ import  APIService  from '../../services/APISevice'
 import ActorThumb from './actor-thumb/ActorThumb'
 import {ActorListInterface} from '../../../../interfaces/ActorList'
 import { actorListPlaceholder } from '../../actorListPlaceholder'
+import {Crew, Cast} from '../../../../interfaces/ActorList'
+
 type Props = {
   id:number
 }
+
+const castArray:Cast[] =[];
+
 const ActorsList:React.FC<Props>  = ({id}) => {
-  const [actorList, setActorList] = useState<ActorListInterface>(actorListPlaceholder)
+  const [actorList, setActorList] = useState<Cast[]>(castArray)
   useEffect(() => {
       async function fetchMovie () {
+          setActorList(castArray)
           const actorListIDS = await APIService.getActorList(id);
-          setActorList(actorListIDS);
+          const filteredActorList = actorListIDS.cast.filter((actor, index, self) =>
+            index === self.findIndex((selfActor) => selfActor.id === actor.id)
+          );
+          setActorList(filteredActorList);
       }
       fetchMovie()
   }, [id])
@@ -21,7 +30,7 @@ const ActorsList:React.FC<Props>  = ({id}) => {
         <h1>Cast</h1>
         {console.log(actorList)}
         <div className="movie-list">
-            {actorList.cast.map((actor:any) => <ActorThumb key={Number(actor.id)} actor={actor}/>)}
+            {actorList.map((actor:any) => <ActorThumb key={Number(actor.id)} actor={actor} role={actor.character}/>)}
         </div>
     </div>
   )
--- moviematcher/src/components/actors-list/actor-thumb/ActorThumb.tsx
+++ moviematcher/src/components/actors-list/actor-thumb/ActorThumb.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import './ActorList.css'
 import { Cast } from '../../../../../interfaces/ActorList'
 type Props = {
-  actor:Cast
+  actor:Cast,
+  role:string
 }
-const ActorThumb:React.FC<Props> = ({actor}) => {
+const ActorThumb:React.FC<Props> = ({actor, role}) => {
   return (
        <div className="actor-thumb">
           {actor.profile_path && 
@@ -12,12 +13,22 @@ const ActorThumb:React.FC<Props> = ({actor}) => {
             <img className='actor-thumb-img' src={`https://image.tmdb.org/t/p/w500${actor.profile_path}`} alt='movie poster' /> 
               <div className='actor-thumb-img-background'>
                 <p> {actor.name}</p>
+                {role && 
+                <>
+                  <p>as</p>
+                  <p>{role}</p>
+                </>}
               </div>  
           </div>}
           {!actor.profile_path && 
           <div className='actor-thumb-img-background-alt'>
-          <p> {actor.name}</p>
-        </div>  }
+            <p> {actor.name}</p>
+            {role &&
+            <> 
+              <p>as</p>
+              <p>{role}</p>
+            </>}
+          </div>}
         </div> 
   )
 }
