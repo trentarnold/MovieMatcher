0f4f734c0ecb4f9260991ed8d51746b41fd7965b
trentarnold <trentarnold8@gmail.com>
2021-11-30 14:00:48 -0800

    premerge commit

--- interfaces/movieInterface.ts
+++ interfaces/movieInterface.ts
@@ -1,19 +1,24 @@
 export interface Movie {
-  adult: boolean,
-  backdrop_path: string,
-  genre_ids: number[],
-  id: number,
-  original_language: string,
-  original_title: string,
-  overview: string,
-  popularity: number,
-  poster_path: string,
-  release_date: string,
-  title: string,
-  video: boolean,
-  vote_average: number,
-  vote_count: number
+    adult: Boolean,
+    backdrop_path: String,
+    genre_ids: Array<Number>,
+    id: number,
+    original_language: String,
+    original_title: String,
+    overview: String,
+    popularity: Number,
+    poster_path: String,
+    release_date: String,
+    title: String,
+    video: Boolean,
+    vote_average: number,
+    vote_count: number
 }
+
 export interface Results {
-  results: Movie[]
+  results: Movie[],
+  page?: Number,
+  total_pages?:Number,
+  total_results?:Number
 }
+
