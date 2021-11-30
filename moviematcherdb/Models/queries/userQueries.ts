169a4ee5ed2cd949d5093e6cbba624c0ae1777ae
Sam Welch <samcwelch@gmail.com>
2021-11-30 17:23:12 -0500

    adding: user auth middleware, 1 controller working!

--- moviematcherdb/Models/queries/userQueries.ts
+++ moviematcherdb/Models/queries/userQueries.ts
@@ -1,9 +1,14 @@
 import User, { UserInstance, UserAttributes } from '../user';
 
-export async function fetchUserQuery(id: number): Promise<UserInstance | null> {
-  return await User.findOne({where: { id: id }});
+export async function fetchUserQuery(id: number) {
+  return await User.findOne({where: { id: id }}).then((user: any) => {
+    const userInstance = user.dataValues;
+    return userInstance;
+  })
 }
 
 export async function createUserQuery(user: UserAttributes): Promise<UserInstance> {
   return await User.create(user);
 }
+
+
