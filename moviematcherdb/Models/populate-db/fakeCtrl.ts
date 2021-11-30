1187319879496ad1abd72d566c89faf7471def79
Sam Welch <samcwelch@gmail.com>
2021-11-30 11:46:04 -0500

    adding: files for adding mock data to DB, with associations b/w user and each other table

---
+++ moviematcherdb/Models/populate-db/fakeCtrl.ts
@@ -0,0 +1,19 @@
+import { connectDB } from '../index'
+import User from '../user';
+
+connectDB();
+
+async function find() {
+  // const res = await User.findAll()
+  // console.log(res)
+  const res = await User.findOne<any>({where: {id: 1}, include:["watched_movies", "ratings", "friends", "whitelist", "blacklist"]})
+  if (res) {
+    console.log('+++++++++++ watched movies', res.dataValues.watched_movies.map((m:any) => m.dataValues))
+    console.log('+++++++++++ ratings', res.dataValues.ratings.map((m:any) => m.dataValues))
+    console.log('+++++++++++ friends', res.dataValues.friends.map((m:any) => m.dataValues))
+    console.log('+++++++++++ whitelist', res.dataValues.whitelist.map((m:any) => m.dataValues))
+    console.log('+++++++++++ blacklist', res.dataValues.blacklist.map((m:any) => m.dataValues))
+  }
+}
+
+find();
