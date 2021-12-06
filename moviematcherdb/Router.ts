e3ae526ed280470033a1ac185d44ac59fabda2e2
Caleb <calebcmcgaha@gmail.com>
2021-12-06 13:59:22 -0600

    added routes for api calls

--- moviematcherdb/Router.ts
+++ moviematcherdb/Router.ts
@@ -28,7 +28,7 @@ const {
 router.put('/user/profile',authMiddleware, updatePictureMiddleware, updateUser);
 router.get('/user/profile', authMiddleware, getUser);
 router.post('/user/otherUser', authMiddleware, getSpecificUser) // Not for user calls, internal use only!
-router.post('user/getByUsername', authMiddleware, getByUsername) //Only for Socket.IO room. Need to fix if time allows
+router.post('/user/getByUsername', authMiddleware, getByUsername) //Only for Socket.IO room. Need to fix if time allows
 router.get('/user/allPeople', authMiddleware, getAllPeople)
 router.get('/user/friends',authMiddleware, getFriends);
 router.post('/user/create', checkUsernameMiddleware, setPictureMiddleware ,createUser);
@@ -47,17 +47,30 @@ router.post('/blacklist', authMiddleware, getBlacklist);
 
 //Movie Controller Routes
 const {
-getWatchedMovie,
-addWatchedMovie,
-movieWatchCount,
-APIServiceHandler
+  getWatchedMovie,
+  addWatchedMovie,
+  movieWatchCount,
+  getFileredMovies,
+  getPopularMovies,
+  getUpcomingMovies,
+  getActorsList,
+  getStreamProviders,
+  getSimilarMovies,
+  getActorsDetails,
+  getCombinedCredits
 
 } = require('./Controllers/MovieController');
 router.post('/user/watched', authMiddleware, getWatchedMovie)
 router.post('/user/addWatched', authMiddleware, addWatchedMovie)
 router.post('/user/movieCount', authMiddleware, movieWatchCount)
-router.post('/movies/APIservice?:params', APIServiceHandler )
-
+router.post('/movies/APIservice?:params', getFileredMovies )
+router.get('/movies/Popular', getPopularMovies); //
+router.get('/movies/Upcoming', getUpcomingMovies); //
+router.get('/movies/ActorList?:params', getActorsList); //
+router.get('/movies/Providers', getStreamProviders); //
+router.get('/movies/Similar', getSimilarMovies); //
+router.get('/movies/ActorDetails?:params', getActorsDetails);//
+router.get('/movies/CombinedCredits', getCombinedCredits);
 //Action Controller Routes
 const {
   getActivity,
